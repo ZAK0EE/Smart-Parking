faa752075d7fad06b6c6f4b9a15604b22f0b6977
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-07-25 12:27:20 +0300

    Fixed LCD driver

--- Inc/HAL/LCD.h
+++ Inc/HAL/LCD.h
@@ -8,63 +8,80 @@
 #ifndef LCD_H_
 #define LCD_H_
 
-
-#include <stdint.h>
-
+#define LINE_DIGITS 16
+#include "stdint.h"
 #include "MCAL/Stm32_F103C6_GPIO.h"
 
-#define LCD_PORT GPIOA
-//#define LCD_DDR DDRA
+typedef enum{
+	CURSOR_LEFT  = 0x00,
+	CURSOR_RIGHT = 0x04,
+	DISP_LEFT	 = 0x08,
+	DISP_RIGHT	 = 0x0c
+	}e_ShiftDir_t;
+typedef enum{
+	NOSHIFT_DECREMENT=0,
+	SHIFT_DECREMENT=1,
+	NOSHIFT_INCREMENT=2,
+	SHIFT_INCREMENT=3
+	}e_EntryMode_t;
+typedef enum{
+	OFFDISP_OFFCURSOR	=	0x00,
+	ONDISP_OFFCURSOR	=	0x04,
+	ONDISP_ONCURSOR		=	0x06,
+	ONDISP_BLINKCURSOR	=	0x07
+	}e_DispCtrl_t;
+typedef enum{
+	FOURBIT_1LINE_7DOT	=	0x00,
+	FOURBIT_1LINE_10DOT	=	0x04,
+	FOURBIT_2LINE_7DOT	=	0x08,
+	FOURBIT_2LINE_10DOT	=	0x0C,
+	EIGHTBIT_1LINE_7DOT	=	0x10,
+	EIGHTBIT_1LINE_10DOT=	0x14,
+	EIGHTBIT_2LINE_7DOT	=	0x18,
+	EIGHTBIT_2LINE_10DOT=	0x1C
+	}e_OperMode_t;
+	// in this version i separated the struct of the control pins from the struct of data pins in case if the the control pins were in another port
+typedef struct {
+	GPIO_TypeDef 	*GPIOX;
+	uint16_t 		DataPinStart;				//the pin number of the lowest data pin
+	}s_LCDDataPins_t;
+typedef struct {
+	GPIO_TypeDef 	*GPIOX;
+	uint16_t 		rspin;						//register select pin
+	uint16_t 		enpin;						//enable pin
+	}s_LCDCtrlPins_t;
+typedef struct{
+	s_LCDCtrlPins_t s_LCDCtrlPins;
+	s_LCDDataPins_t	s_LCDDataPins ;
+	e_EntryMode_t	e_EntryMode;
+	e_DispCtrl_t	e_DispCtrl;
+	e_OperMode_t	e_OperMode;
+	uint8_t 		Ch_Counter;
+	}s_LCD_Config_t;
+typedef enum {
+	COMMAND,
+	DATA
+}e_LCDCharType_t;
+// used to initialize the LCD , the config must be done first
+void LCD_init(s_LCD_Config_t  *  LCD );
+//displaying character or sending command
+void LCD_sendchar(s_LCD_Config_t  *  LCD ,unsigned char data ,e_LCDCharType_t e_LCDCharType);
+//the whole string is displayed
+void LCD_sendstring(s_LCD_Config_t  * LCD , const char *  data);
+//it's not complete yet , but it's suppose to check the LCD busy flag but my code didn't work in 4bit mode
+void LCDIsbusy(s_LCD_Config_t  * LCD);
+// clear
+void LCD_clearscreen(s_LCD_Config_t  * LCD);
+// return to 0, 0
+void LCD_returnhome(s_LCD_Config_t  * LCD);
+// well, you got that
+void LCD_gotoxy(s_LCD_Config_t  * LCD , unsigned char x , unsigned char y);
+//used to build your custom character
+void LCD_buildcustom(s_LCD_Config_t  * LCD ,unsigned char location ,const unsigned char * pattern );
+// display it now
+void LCD_dispcustom(s_LCD_Config_t  * LCD ,unsigned char location);
+//shift display and/or cursor
+void LCD_shift(s_LCD_Config_t  * LCD ,e_ShiftDir_t e_ShiftDir);
 
-#define LCDCR_PORT GPIOB
-//#define LCDCR_DDR DDRB
-
-#define LCD_RS GPIO_PIN_10
-#define LCD_RW GPIO_PIN_1
-#define LCD_EN GPIO_PIN_0
-
-//#define LCD_8BIT_MODE
-#define LCD_4BIT_MODE
-
-#define LCD_REGISTER_SELECT_PIN						(0)
-#define LCD_READ_WRITE_PIN							(1)
-#define LCD_ENABLE_PIN								(2)
-#define LCD_REGISTER_SELECT_ENABLE					(1)
-#define LCD_REGISTER_SELECT_DISABLE					(0)
-#define READ_FROM_LCD								(1)
-#define WRITE_TO_LCD								(0)
-#define LCD_ENABLE									(1)
-#define LCD_DISABLE									(0)
-#define LCD_FIRST_LINE								(0)
-#define LCD_SECOND_LINE								(1)
-#define LCD_FUNCTION_8BIT_2LINES   					(0x38)
-#define LCD_FUNCTION_4BIT_2LINES   					(0x28)
-#define LCD_MOVE_DISP_RIGHT       					(0x1C)
-#define LCD_MOVE_DISP_LEFT   						(0x18)
-#define LCD_MOVE_CURSOR_RIGHT   					(0x14)
-#define LCD_MOVE_CURSOR_LEFT 	  					(0x10)
-#define LCD_DISP_OFF   								(0x08)
-#define LCD_DISP_ON_CURSOR   						(0x0E)
-#define LCD_DISP_ON_CURSOR_BLINK   					(0x0F)
-#define LCD_DISP_ON_BLINK   						(0x0D)
-#define LCD_DISP_ON   								(0x0C)
-#define LCD_ENTRY_DEC   							(0x04)
-#define LCD_ENTRY_DEC_SHIFT   						(0x05)
-#define LCD_ENTRY_INC_   							(0x06)
-#define LCD_ENTRY_INC_SHIFT   						(0x07)
-#define LCD_BEGIN_AT_FIRST_RAW						(0x80)
-#define LCD_BEGIN_AT_SECOND_RAW						(0xC0)
-#define LCD_CLEAR_SCREEN							(0x01)
-#define LCD_ENTRY_MODE								(0x06)
-#define LCD_EN_4BIT									(0x02)
-
-
-void LCD_Init(void);
-void LCD_Write_Comand(unsigned char command);
-void LCD_Write_Char(unsigned char data);
-void LCD_Write_String(char* data);
-void LCD_Write_CustomChar(void);
-void LCD_Is_Busy(void);
-void LCD_Clear_Screen(void);
 
 #endif /* LCD_H_ */
--- Src/HAL/LCD.c
+++ Src/HAL/LCD.c
@@ -1,264 +1,190 @@
 /*
- * lcd.c
+ * LCD.c
  *
- * Created: 22/07/31 10:02:04 AM
- *  Author: Ziad Gamalelden
+ * Created: 6/30/2021 8:56:53 PM
+ *  Author: Mostafa El-Flall
  */
+
 #include "HAL/LCD.h"
-#include <string.h>
+
+#define BITSET(reg,bit) (reg |=(1<< bit))
+#define BITCLEAR(reg,bit) (reg &= ~(1<<bit))
+#define ClEAR_SCREEN			0x01
+#define RETURN_HOME				0x02
+#define ENTRY_MODE_SET			0x04
+#define	DISPLAY_CONTROL			0x08
+#define CURSOR_DISPLAY_SHIFT	0x10
+#define FUNCTION_SET			0x20
+#define SET_CG_ADDRESS			0x40
+#define SET_DD_ADDRESS			0x80
 
 
-#ifdef LCD_4BIT_MODE
-#define LCD_DATASHIFT 4
-#else
-#define LCD_DATASHIFT 0
-#endif
-
-static void delay(int a)
-{
-	for(int i = 0; i < a; i++)
-	{
-		for(int j = 0; j < 255; j++);
+static void LCD_delay(int ms){
+	volatile uint32_t counter =0;
+	for(int i =0;i<ms;i++){
+		for(;counter<1500;counter++);
 	}
 }
+//void MCAL_GPIO_WritePin(GPIO_TypeDef * GPIOx,uint16_t PinNumber,uint8_t Vlaue);
 
-void LCD_KICK()
-{
-	//LCDCR_PORT |= (1 << LCD_EN); // Set EN pin
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_EN, GPIO_PIN_SET);
-	// _delay_us(1);
-	delay(1);
-	//LCDCR_PORT &= ~(1 << LCD_EN); // Reset EN pin
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_EN, GPIO_PIN_RESET);
-
-
+void LCD_kick(s_LCD_Config_t  * LCD){
+	//BITSET(*(LCD->s_LCDCtrlPins.p_PortReg),LCD->s_LCDCtrlPins.enpin);									//enable pulse
+	MCAL_GPIO_WritePin(LCD->s_LCDCtrlPins.GPIOX,LCD->s_LCDCtrlPins.enpin,GPIO_PIN_SET);
+	LCD_delay(1);
+	MCAL_GPIO_WritePin(LCD->s_LCDCtrlPins.GPIOX,LCD->s_LCDCtrlPins.enpin,GPIO_PIN_RESET);
 }
 
-void LCD_Clear_Screen(void)
-{
-	LCD_Write_Comand(LCD_CLEAR_SCREEN);
-
-}
-void LCD_Is_Busy(void)
-{
-
-	//LCD_DDR &= ~(0xFF << LCD_DATASHIFT);
-	LCD_PORT->CRL = (uint32_t)(0x88888888 << LCD_DATASHIFT);
-
-
-
-	//LCDCR_PORT |= (1 << LCD_RW); // RW pin is read
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RW, GPIO_PIN_SET);
-	//LCDCR_PORT &= ~(1 << LCD_RS); // RS pin is write
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RS, GPIO_PIN_RESET);
-
-	LCD_KICK();
-
-	//LCD_DDR = 0xFF;
-	LCD_PORT->CRL = 0x88888888; // LCD D[0:7] as input
-	// LCDCR_PORT &= ~(1 << LCD_RW);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RW, GPIO_PIN_RESET);
-
-}
-
-void LCD_GotoXY(int line, int pos)
-{
-
-	if(line == 0)
+void LCD_init(s_LCD_Config_t  *  LCD ){
+	//*(LCD->s_LCDCtrlPins.p_DataDireReg) |= (1<<LCD->s_LCDCtrlPins.enpin) | (1<<LCD->s_LCDCtrlPins.rspin) | (1<<LCD->s_LCDCtrlPins.rwpin);
+	GPIO_PinConfig_t pin = {LCD->s_LCDCtrlPins.enpin ,GPIO_MODE_OUTPUT_PP, GPIO_SPEED_10M};
+	MCAL_GPIO_Init(LCD->s_LCDCtrlPins.GPIOX,&pin);
+	pin.GPIO_PinNumber = LCD->s_LCDCtrlPins.rspin;
+	MCAL_GPIO_Init(LCD->s_LCDCtrlPins.GPIOX,&pin);
+	LCD->Ch_Counter = 0;
+	if(LCD->e_OperMode < EIGHTBIT_1LINE_7DOT)	// 4 bits
 	{
-		if(pos > 15 || pos < 0)
-			return;
-		LCD_Write_Comand(LCD_BEGIN_AT_FIRST_RAW + pos);
-	}
-	else if(line == 1)
-	{
-		if(pos > 15 || pos < 0)
-			return;
-		LCD_Write_Comand(LCD_BEGIN_AT_SECOND_RAW + pos);
-	}
-}
-
-void LCD_Init(void)
-{
-	//_delay_ms(20);
-	delay(1);
-	LCD_Is_Busy();
-
-	//LCDCR_DDR |= (1 << LCD_EN) | (1 << LCD_RW) | (1 << LCD_RS);
-	GPIO_PinConfig_t PinConfig;
-	PinConfig.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
-	PinConfig.GPIO_PinNumber = LCD_EN;
-	PinConfig.GPIO_Speed = GPIO_SPEED_10M;
-	MCAL_GPIO_Init(LCDCR_PORT, &PinConfig);
-
-	PinConfig.GPIO_PinNumber = LCD_RW;
-	MCAL_GPIO_Init(LCDCR_PORT, &PinConfig);
-
-	PinConfig.GPIO_PinNumber = LCD_RS;
-	MCAL_GPIO_Init(LCDCR_PORT, &PinConfig);
-
-
-	// LCDCR_PORT &= ~((1 << LCD_EN) | (1 << LCD_RW) | (1 << LCD_RS));
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_EN, GPIO_PIN_RESET);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RW, GPIO_PIN_RESET);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RS, GPIO_PIN_RESET);
-	// 	LCD_DDR = 0xFF;
-	LCD_PORT->CRL = 0x88888888;
-	LCD_Clear_Screen();
-
-	//_delay_ms(15);
-	delay(2);
-
-#ifdef LCD_8BIT_MODE
-	LCD_Write_Comand(LCD_FUNCTION_8BIT_2LINES);
-#endif
-#ifdef LCD_4BIT_MODE
-	LCD_Write_Comand(LCD_EN_4BIT);
-	LCD_Write_Comand(LCD_FUNCTION_4BIT_2LINES);
-
-#endif
-
-	LCD_Write_Comand(LCD_ENTRY_MODE);
-	LCD_Write_Comand(LCD_BEGIN_AT_FIRST_RAW);
-	LCD_Write_Comand(LCD_DISP_ON_CURSOR_BLINK);
-
-}
-
-
-
-void LCD_Write_Comand(unsigned char command)
-{
-	LCD_Is_Busy();
-
-#ifdef LCD_8BIT_MODE
-	//LCD_PORT = command;
-	MCAL_GPIO_WritePort(LCD_PORT, command);
-
-	//	LCDCR_PORT &= ~((1 << LCD_RW) | (1 << LCD_RS));
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RW, GPIO_PIN_RESET);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RS, GPIO_PIN_RESET);
-	LCD_KICK();
-#endif
-#ifdef LCD_4BIT_MODE
-	//LCD_PORT = (LCD_PORT & 0x0F) | (command & 0xF0); 0xFFFFFFFF
-	uint32_t LCDValue = MCAL_GPIO_ReadPort(LCD_PORT);
-	LCDValue = (LCDValue & 0xFFFFFF0F) | (command & 0xFFFFFFF0);
-	MCAL_GPIO_WritePort(LCD_PORT, LCDValue);
-
-	//LCDCR_PORT &= ~((1 << LCD_RW) | (1 << LCD_RS));
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RW, GPIO_PIN_RESET);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RS, GPIO_PIN_RESET);
-
-	LCD_KICK();
-	//_delay_us(200);
-	delay(1);
-
-	//LCD_PORT = (LCD_PORT & 0x0F) | (command << 4);
-	LCDValue = MCAL_GPIO_ReadPort(LCD_PORT);
-	LCDValue = (LCDValue & 0xFFFFFF0F) | (command << 4);
-	MCAL_GPIO_WritePort(LCD_PORT, LCDValue);
-
-	//LCDCR_PORT &= ~((1 << LCD_RW) | (1 << LCD_RS));
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RW, GPIO_PIN_RESET);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RS, GPIO_PIN_RESET);
-
-	LCD_KICK();
-	//_delay_ms(2);
-	delay(2);
-
-
-#endif
-}
-
-
-
-void LCD_Write_Char(unsigned char data)
-{
-
-
-#ifdef LCD_8BIT_MODE
-	LCD_Is_Busy();
-	//LCD_PORT = data;
-	MCAL_GPIO_WritePort(LCD_PORT, data);
-	//LCDCR_PORT &= ~(1 << LCD_RW);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RW, GPIO_PIN_RESET);
-	//LCDCR_PORT |= (1 << LCD_RS);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RS, GPIO_PIN_SET);
-
-	LCD_KICK();
-#endif
-#ifdef LCD_4BIT_MODE
-	//LCD_PORT = (LCD_PORT & 0x0F) | (data & 0xF0);
-	uint32_t LCDValue = MCAL_GPIO_ReadPort(LCD_PORT);
-	LCDValue = (LCDValue & 0xFFFFFF0F) | (data & 0xFFFFFFF0);
-	MCAL_GPIO_WritePort(LCD_PORT, LCDValue);
-
-	//LCDCR_PORT &= ~(1 << LCD_RW);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RW, GPIO_PIN_RESET);
-
-	//LCDCR_PORT |= (1 << LCD_RS);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RS, GPIO_PIN_SET);
-
-	LCD_KICK();
-
-	//LCD_PORT = (LCD_PORT & 0x0F) | (data << 4);
-	LCDValue = MCAL_GPIO_ReadPort(LCD_PORT);
-	LCDValue = (LCDValue & 0xFFFFFF0F) | (data << 4);
-	MCAL_GPIO_WritePort(LCD_PORT, LCDValue);
-
-	//LCDCR_PORT &= ~(1 << LCD_RW);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RW, GPIO_PIN_RESET);
-
-	//LCDCR_PORT |= (1 << LCD_RS);
-	MCAL_GPIO_WritePin(LCDCR_PORT, LCD_RS, GPIO_PIN_SET);
-	LCD_KICK();
-	//_delay_us(100);
-	delay(1);
-
-
-#endif
-}
-
-void LCD_Write_CustomChar()
-{
-	LCD_Write_Comand(64);
-	LCD_Write_Char(0xff);
-	LCD_Write_Char(0xff);
-	LCD_Write_Char(0xff);
-	LCD_Write_Char(0xff);
-	LCD_Write_Char(0xff);
-	LCD_Write_Char(0xff);
-	LCD_Write_Char(0xff);
-	LCD_Write_Char(0xff);
-
-	LCD_Write_Comand(LCD_BEGIN_AT_FIRST_RAW);
-	LCD_Write_Char(0);
-
-}
-
-
-void LCD_Write_String(char* data)
-{
-	int count = 0;
-	while(*data != 0)
-	{
-		if(count == 16)
-		{
-			LCD_GotoXY(1, 0);
+		//*(LCD->s_LCDDataPins.p_DataDireReg) |= (0x0F << LCD->s_LCDDataPins.DataPinStart);
+		for(uint8_t i =0;i<4;i++){
+			pin.GPIO_PinNumber = LCD->s_LCDDataPins.DataPinStart << i;
+			MCAL_GPIO_Init(LCD->s_LCDDataPins.GPIOX,&pin);
 		}
-		else if (count == 32)
-		{
-			LCD_Clear_Screen();
-			count = 0;
-			LCD_GotoXY(0, 0);
+	}
+	else
+	{
+		for(uint8_t i =0;i<8;i++){
+			pin.GPIO_PinNumber = LCD->s_LCDDataPins.DataPinStart << i;
+			MCAL_GPIO_Init(LCD->s_LCDDataPins.GPIOX,&pin);
+		}
+	}
+	LCD_sendchar(LCD ,RETURN_HOME , COMMAND );
+	LCD_sendchar(LCD ,FUNCTION_SET + LCD->e_OperMode , COMMAND );
+	LCD_sendchar(LCD ,DISPLAY_CONTROL + LCD->e_DispCtrl , COMMAND );
+	LCD_sendchar(LCD ,ENTRY_MODE_SET + LCD->e_EntryMode , COMMAND );
+	LCD_clearscreen(LCD);
+	LCD_returnhome(LCD);
+	LCD_sendchar(LCD ,SET_DD_ADDRESS , COMMAND );
+}
+void LCD_sendchar(s_LCD_Config_t  *  LCD ,unsigned char data ,e_LCDCharType_t e_LCDCharType)
+{
+	LCDIsbusy(LCD);
+	if(LCD->e_OperMode < EIGHTBIT_1LINE_7DOT)	// 4 bits
+	{
+		//void MCAL_GPIO_WriteGroup(GPIO_TypeDef * GPIOx,uint16_t Vlaue,uint16_t mask);
+		//*(LCD->s_LCDDataPins.p_PortReg) &= ~(0x0F << LCD->s_LCDDataPins.DataPinStart);						// clear the data pins
+		//*(LCD->s_LCDDataPins.p_PortReg) = (((data & 0xF0) >> 4) << LCD->s_LCDDataPins.DataPinStart);		//the high nibble
+		MCAL_GPIO_WriteGroup(
+				LCD->s_LCDDataPins.GPIOX
+				,((data & 0xF0) >> 4)*LCD->s_LCDDataPins.DataPinStart
+				,0x0F*LCD->s_LCDDataPins.DataPinStart);
+		if(e_LCDCharType == COMMAND)
+			//BITCLEAR(*(LCD->s_LCDCtrlPins.p_PortReg),LCD->s_LCDCtrlPins.rspin);
+			MCAL_GPIO_WritePin(LCD->s_LCDCtrlPins.GPIOX,LCD->s_LCDCtrlPins.rspin,GPIO_PIN_RESET);
+		else{
+			//BITSET(*(LCD->s_LCDCtrlPins.p_PortReg),LCD->s_LCDCtrlPins.rspin);
+			MCAL_GPIO_WritePin(LCD->s_LCDCtrlPins.GPIOX,LCD->s_LCDCtrlPins.rspin,GPIO_PIN_SET);
 		}
 
-		LCD_Write_Char(*data);
-		count++;
-		data++;
+		LCD_kick(LCD);
 
+		//*(LCD->s_LCDDataPins.p_PortReg) &= ~(0x0F << LCD->s_LCDDataPins.DataPinStart);
+		//*(LCD->s_LCDDataPins.p_PortReg) |= ((data & 0x0F) << LCD->s_LCDDataPins.DataPinStart);
+		MCAL_GPIO_WriteGroup(LCD->s_LCDDataPins.GPIOX
+				,(data & 0x0F)*LCD->s_LCDDataPins.DataPinStart
+				,0x0F*LCD->s_LCDDataPins.DataPinStart);
 
+		LCD_kick(LCD);
+		LCD_delay(1);
+	}
+	else
+	{						//8bit mode
+		//*(LCD->s_LCDDataPins.p_PortReg) &= ~(0xFF << LCD->s_LCDDataPins.DataPinStart);
+		//*(LCD->s_LCDDataPins.p_PortReg) |= (data << LCD->s_LCDDataPins.DataPinStart);
+		MCAL_GPIO_WriteGroup(LCD->s_LCDDataPins.GPIOX
+				,(data & 0xFF)*LCD->s_LCDDataPins.DataPinStart
+				,0xFF*LCD->s_LCDDataPins.DataPinStart);
+		if(e_LCDCharType == COMMAND)
+			//BITCLEAR(*(LCD->s_LCDCtrlPins.p_PortReg),LCD->s_LCDCtrlPins.rspin);
+			MCAL_GPIO_WritePin(LCD->s_LCDCtrlPins.GPIOX,LCD->s_LCDCtrlPins.rspin,GPIO_PIN_RESET);
+		else{
+			//BITSET(*(LCD->s_LCDCtrlPins.p_PortReg),LCD->s_LCDCtrlPins.rspin);
+			MCAL_GPIO_WritePin(LCD->s_LCDCtrlPins.GPIOX,LCD->s_LCDCtrlPins.rspin,GPIO_PIN_SET);
+
+		}
+		LCD_kick(LCD);
+	}
+	if(e_LCDCharType == DATA){
+		LCD->Ch_Counter ++;
+			if(LCD->Ch_Counter==LINE_DIGITS)
+				LCD_gotoxy(LCD,0,1);
+			else if (LCD->Ch_Counter==2*LINE_DIGITS)
+				LCD_gotoxy(LCD,0,2);
+			else if (LCD->Ch_Counter==3*LINE_DIGITS)
+				LCD_gotoxy(LCD,0,3);
+		}
+}
+void LCD_sendstring(s_LCD_Config_t  * LCD , const char *  data)
+{
+	for(;*data != '\0';data++){
+		LCD_sendchar(LCD,*data,DATA);
 	}
 }
+void LCDIsbusy(s_LCD_Config_t  * LCD)
+{
+	//currently I'm working on reading the actual busy flag
+	//for the end of the dev process xD let it simply a delay.
+	LCD_delay(1);
+}
 
+void LCD_clearscreen(s_LCD_Config_t  * LCD)
+{
+	LCD_sendchar(LCD , ClEAR_SCREEN , COMMAND );
+	LCD->Ch_Counter=0;
+}
+void LCD_returnhome(s_LCD_Config_t  * LCD)
+{
+	LCD_sendchar(LCD , RETURN_HOME , COMMAND );
+	LCD->Ch_Counter=0;
+}
+void LCD_gotoxy(s_LCD_Config_t  * LCD , unsigned char x , unsigned char y)
+{
+	uint32_t address;
+	switch(y)
+	{
+	case 0:
+		address = x;
+		break;
+	case 1:
+		address = 0x40+x;
+		break;
+	case 2:
+		address = 0x14+x;
+		break;
+	case 3:
+		address = 0x54+x;
+		break;
+	}
+	LCD->Ch_Counter=y*LINE_DIGITS+x;
+	LCD_sendchar(LCD,SET_DD_ADDRESS+address,COMMAND);
+}
+void LCD_buildcustom(s_LCD_Config_t  * LCD ,unsigned char location ,const unsigned char * pattern )
+{
+	unsigned char i=0;
+	if(location<8){
+		LCD_sendchar(LCD,0x40+(location*8),COMMAND);
+		for(i=0;i<8;i++)
+			LCD_sendchar(LCD,pattern[ i ],DATA);
+	}
+}
+void LCD_dispcustom(s_LCD_Config_t  * LCD ,unsigned char location){
+	LCD_sendchar(LCD,location,DATA);
+}
+void LCD_shift(s_LCD_Config_t  * LCD ,e_ShiftDir_t e_ShiftDir){
+	LCD_sendchar(LCD ,CURSOR_DISPLAY_SHIFT + e_ShiftDir , COMMAND );
+}
+
+void Wait()
+{
+	int i=0;
+	for(i=0;i<25000;i++);
+}
 
