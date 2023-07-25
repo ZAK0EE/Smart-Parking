4bf256e14353f4ab0ed5bba5ee3b40b893a48d68
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-07-25 17:49:12 +0300

    Fixed Keypad to match with array sizes

--- Inc/HAL/Keypad.h
+++ Inc/HAL/Keypad.h
@@ -9,24 +9,23 @@
 #define HAL_KEYPAD_H_
 
 
-#include "Stm32_F103C6_GPIO.h"
+#include "MCAL/Stm32_F103C6_GPIO.h"
 
 #define KPAD_PORT GPIOB
 
 
 // Keypad Rows
-#define KPAD_R0 GPIO_PIN_3
-#define KPAD_R1 GPIO_PIN_4
-#define KPAD_R2 GPIO_PIN_5
-#define KPAD_R3 GPIO_PIN_6
+#define KPAD_R0 GPIO_PIN_0
+#define KPAD_R1 GPIO_PIN_1
+#define KPAD_R2 GPIO_PIN_3
+#define KPAD_R3 GPIO_PIN_4
 
 // Keypad Columns
-#define KPAD_C0 GPIO_PIN_12
-#define KPAD_C1 GPIO_PIN_13
-#define KPAD_C2 GPIO_PIN_14
-#define KPAD_C3 GPIO_PIN_15
+#define KPAD_C0 GPIO_PIN_5
+#define KPAD_C1 GPIO_PIN_6
+#define KPAD_C2 GPIO_PIN_7
 
-void KPAD_Init();
+void KPAD_init();
 
 char KPAD_Get_Char();
 
--- Src/HAL/Keypad.c
+++ Src/HAL/Keypad.c
@@ -5,26 +5,26 @@
  *  Author: Ziad Gamalelden
  */
 
-#include "keypad.h"
+#include "HAL/Keypad.h"
 
-uint16_t Col[4] = {KPAD_C0, KPAD_C1, KPAD_C2, KPAD_C3};
-uint16_t Row[4] = {KPAD_R0, KPAD_R1, KPAD_R2, KPAD_R3};
+uint16_t Col[] = {KPAD_C0, KPAD_C1, KPAD_C2};
+uint16_t Row[] = {KPAD_R0, KPAD_R1, KPAD_R2, KPAD_R3};
 
-char KPAD_Chars[4][4] = {
-							{'7', '8', '9', '/'},
-							{'4', '5', '6', '*'},
-							{'1', '2', '3', '-'},
-							{'c', '0', '=', '+'}
+char KPAD_Chars[4][3] = {
+							{'*', '0', '#'},
+							{'7', '8', '9'},
+							{'4', '5', '6'},
+							{'1', '2', '3'}
 						};
 
 
 
-void KPAD_Init()
+void KPAD_init()
 {
 
 	GPIO_PinConfig_t PinConfig;
 	PinConfig.GPIO_Mode = GPIO_MODE_INPUT_PD;
-	for(int Ri = 0; Ri < 4; Ri++)
+	for(int Ri = 0; Ri < (sizeof(Row) / sizeof(Row[0])); Ri++)
 	{
 		PinConfig.GPIO_PinNumber = Row[Ri];
 		MCAL_GPIO_Init(KPAD_PORT, &PinConfig);
@@ -34,33 +34,31 @@ void KPAD_Init()
 	PinConfig.GPIO_Mode = GPIO_MODE_OUTPUT_PP;
 	PinConfig.GPIO_Speed = GPIO_SPEED_10M;
 
-	for(int Ci = 0; Ci < 4; Ci++)
+	for(int Ci = 0; Ci < (sizeof(Col) / sizeof(Col[0])); Ci++)
 	{
 		PinConfig.GPIO_PinNumber = Col[Ci];
 		MCAL_GPIO_Init(KPAD_PORT, &PinConfig);
 	}
 
 
-	for(int Ci = 0; Ci < 4; Ci++)
+	for(int Ci = 0; Ci < (sizeof(Col) / sizeof(Col[0])); Ci++)
 		MCAL_GPIO_WritePin(KPAD_PORT, Col[Ci], GPIO_PIN_SET);
-	for(int Ri = 0; Ri < 4; Ri++)
+	for(int Ri = 0; Ri < (sizeof(Row) / sizeof(Row[0])); Ri++)
 		MCAL_GPIO_WritePin(KPAD_PORT, Row[Ri], GPIO_PIN_SET);
 }
 
 char KPAD_Get_Char()
 {
 	int Ri, Ci;
-	for(Ci = 0; Ci < 4; Ci++)
+	for(Ci = 0; Ci < (sizeof(Col) / sizeof(Col[0])); Ci++)
 	{
 		// All columns HIGH
-		MCAL_GPIO_WritePin(KPAD_PORT, KPAD_C0, GPIO_PIN_SET);
-		MCAL_GPIO_WritePin(KPAD_PORT, KPAD_C1, GPIO_PIN_SET);
-		MCAL_GPIO_WritePin(KPAD_PORT, KPAD_C2, GPIO_PIN_SET);
-		MCAL_GPIO_WritePin(KPAD_PORT, KPAD_C3, GPIO_PIN_SET);
+		for(int nestedCi= 0; nestedCi < (sizeof(Col) / sizeof(Col[0])); nestedCi++)
+			MCAL_GPIO_WritePin(KPAD_PORT, Col[nestedCi], GPIO_PIN_SET);
 
-		// Ground once column
+		// Ground each column
 		MCAL_GPIO_WritePin(KPAD_PORT, Col[Ci], GPIO_PIN_RESET);
-		for(Ri = 0; Ri < 4; Ri++)
+		for(Ri = 0; Ri < (sizeof(Row) / sizeof(Row[0])); Ri++)
 		{
 			if(!MCAL_GPIO_ReadPin(KPAD_PORT, Row[Ri]))
 			{
