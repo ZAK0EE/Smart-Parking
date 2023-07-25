09a3cc55afecbc4bda4b8c840c5324d34ac9c188
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-07-25 23:31:54 +0300

    Added PIR APIs

--- Inc/HAL/PIR.h
+++ Inc/HAL/PIR.h
@@ -8,6 +8,24 @@
 #ifndef HAL_PIR_H_
 #define HAL_PIR_H_
 
+typedef enum
+{
+	PIR_NOT_DETECTED,
+	PIR_DETECTED
+}e_PIRStatus_t;
+
+#define PIR_GPIOx		GPIOA
+#define PIR_ENTRY_PIN	GPIO_PIN_7
+#define PIR_EXIT_PIN	GPIO_PIN_1
+
+
+void PIR_init(void);
+
+e_PIRStatus_t PIR_entry(void);
+
+e_PIRStatus_t PIR_exit(void);
+
+
 
 
 #endif /* HAL_PIR_H_ */
--- Src/HAL/PIR.c
+++ Src/HAL/PIR.c
@@ -5,5 +5,60 @@
  *      Author: Ziad Gamalelden
  */
 
+#include <stdint.h>
+
+#include "HAL/PIR.h"
+#include "MCAL/Stm32_F103C6_GPIO.h"
+
+void PIR_init(void)
+{
+
+	GPIO_PinConfig_t PinConfig;
+	PinConfig.GPIO_Mode = GPIO_MODE_INPUT_PU;
+	PinConfig.GPIO_Speed = GPIO_SPEED_10M;
+
+	// Entry gate PIR
+	PinConfig.GPIO_PinNumber= PIR_ENTRY_PIN;
+	MCAL_GPIO_Init(GPIOA, &PinConfig);
+
+	// Exit gate PIR
+	PinConfig.GPIO_PinNumber= PIR_EXIT_PIN;
+	MCAL_GPIO_Init(GPIOA, &PinConfig);
+
+	MCAL_GPIO_WritePin(PIR_GPIOx, PIR_ENTRY_PIN, GPIO_PIN_SET); //make A0 ground
+	MCAL_GPIO_WritePin(PIR_GPIOx, PIR_EXIT_PIN, GPIO_PIN_SET); //make A11 ground
+}
+
+e_PIRStatus_t PIR_entry(void)
+{
+	e_PIRStatus_t status;
+	uint8_t pinStatus = MCAL_GPIO_ReadPin(PIR_GPIOx, PIR_ENTRY_PIN);
+	if(pinStatus == GPIO_PIN_SET)
+	{
+		status = PIR_DETECTED;
+	}
+	else
+	{
+		status = PIR_NOT_DETECTED;
+	}
+
+	return status;
+}
+
+e_PIRStatus_t PIR_exit(void)
+{
+	e_PIRStatus_t status;
+	uint8_t pinStatus = MCAL_GPIO_ReadPin(PIR_GPIOx, PIR_EXIT_PIN);
+	if(pinStatus == GPIO_PIN_SET)
+	{
+		status = PIR_DETECTED;
+	}
+	else
+	{
+		status = PIR_NOT_DETECTED;
+	}
+
+	return status;
+}
 
 
