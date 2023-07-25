1e475047a53e342e10cee91cfc61c6c612acfd67
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-07-25 23:02:12 +0300

    Added PIR Driver files
    PIR: Passive InfraRed Sensor

---
+++ Inc/HAL/PIR.h
@@ -0,0 +1,13 @@
+/*
+ * PIR.h
+ *
+ *  Created on: Jul 25, 2023
+ *      Author: Ziad Gamalelden
+ */
+
+#ifndef HAL_PIR_H_
+#define HAL_PIR_H_
+
+
+
+#endif /* HAL_PIR_H_ */
---
+++ Src/HAL/PIR.c
@@ -0,0 +1,9 @@
+/*
+ * PIR.c
+ *
+ *  Created on: Jul 25, 2023
+ *      Author: Ziad Gamalelden
+ */
+
+
+
