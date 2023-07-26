227dc16334c88f06978318a4a55140a5828e20dc
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-07-26 20:46:16 +0300

    Removed uneeded code

--- Src/main.c
+++ Src/main.c
@@ -16,69 +16,10 @@
 #include "HAL/HAL.h"
 
 
-
-void delay(int a)
-{
-	for(int i = 0; i < a; i++)
-	{
-		for(int j = 0; j < 255; j++);
-	}
-}
-
-
-uint16_t ch = '5';
-uint16_t ch2 = '6';
-void x(void)
-{
-	MCAL_UART_ReceiveData(USART1, &ch, disable);
-	if(ch == '1')
-	{
-		Servo1_Entry_Gate(SERVO_UP);
-		Alarm_correctID_blink();
-	}
-	else if(ch == '0')
-	{
-		if(PIR_exit() == PIR_NOT_DETECTED)
-		{
-			Servo1_Entry_Gate(SERVO_DOWN);
-			Alarm_correctID_blink();
-			Alarm_correctID_off();
-
-		}
-	}
-	else
-	{
-		Alarm_wrongID_blink();
-		Alarm_wrongID_off();
-
-	}
-
-	MCAL_UART_SendData(USART1, &ch, enable);
-
-}
-
-void yfunc(void)
-{
-	MCAL_UART_ReceiveData(USART2, &ch2, disable);
-	LCD_clearscreen(&LCD_user);
-	LCD_sendstring(&LCD_user, (char*)"Welcome bro");
-	LCD_gotoxy(&LCD_user, 0, 1);
-	LCD_sendstring(&LCD_user, (char*)"Name: ");
-	LCD_gotoxy(&LCD_user, 0, 2);
-	LCD_sendstring(&LCD_user, (char*)"Age: ");
-	LCD_gotoxy(&LCD_user, 0, 3);
-	LCD_sendstring(&LCD_user, (char*)"Degree: ");
-
-	MCAL_UART_SendData(USART2, (uint16_t*)&x, enable);
-
-}
 int main(void)
 {
 	MCAL_init();
 	HAL_init();
-
-
-
 
 
 	while(1)
