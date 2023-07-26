c13a925f2d58b93c5a8f3766340a0c35fa41a5a3
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-07-26 20:44:26 +0300

    Added RFID test code

--- Src/main.c
+++ Src/main.c
@@ -10,6 +10,7 @@
 #include "HAL/Servo_Motor.h"
 #include "HAL/PIR.h"
 #include "HAL/Alarm.h"
+#include "HAL/RFID.h"
 
 #include "MCAL/MCAL.h"
 #include "HAL/HAL.h"
@@ -76,37 +77,15 @@ int main(void)
 	MCAL_init();
 	HAL_init();
 
-	USART_Config_t uart_config;
-	uart_config.BaudRate = UART_BaudRate_115200;
-	uart_config.HwFlowCtl = UART_HwFlowCtl_NONE;
-	uart_config.IRQ_Enable = UART_IRQ_Enable_RXNEIE;
-	uart_config.P_IRQ_CallBack = x;
-	uart_config.Parity = UART_Parity_NONE;
-	uart_config.Payload_Length = UART_Payload_Length_8B;
-	uart_config.StopBits = UART_StopBits_1;
-	uart_config.USART_Mode = UART_Mode_TX_RX;
-	MCAL_UART_Init(USART1, &uart_config);
-	MCAL_UART_GPIO_Set_Pins(USART1);
 
 
-	USART_Config_t uart2CFG;
-
-	uart2CFG.BaudRate = UART_BaudRate_115200;
-	uart2CFG.HwFlowCtl = UART_HwFlowCtl_NONE;
-	uart2CFG.IRQ_Enable = UART_IRQ_Enable_NONE;
-	uart2CFG.P_IRQ_CallBack = yfunc;
-	uart2CFG.Parity = UART_Parity_NONE;
-	uart2CFG.Payload_Length = UART_Payload_Length_8B;
-	uart2CFG.StopBits = UART_StopBits_1;
-	uart2CFG.USART_Mode = UART_Mode_TX_RX;
-	MCAL_UART_Init(USART2, &uart2CFG);
-	MCAL_UART_GPIO_Set_Pins(USART2);
 
 
 	while(1)
 	{
 
-
+		uint16_t x = RFID_exit_Read();
+		LCD_sendchar(&LCD_user, (unsigned char)x, DATA);
 	}
 
 }
