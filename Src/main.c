fdd6d7d64cf55b4d47d17648e4df278f5fd69183
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-07-27 01:08:22 +0300

    Cleaned up main and added required initialization

--- Src/main.c
+++ Src/main.c
@@ -1,32 +1,13 @@
 // Learn-in-depth
 // Ziad Ashraf
 // Mastering_Embedded System online diploma
-#include "MCAL/stm32f103x6.h"
-#include "MCAL/Stm32_F103C6_GPIO.h"
-#include "MCAL/Stm32_F103C6_USART.h"
-#include "MCAL/Stm32_F103C6_Timer.h"
-#include "HAL/LCD.h"
-#include "HAL/KPAD.h"
-#include "HAL/Servo_Motor.h"
-#include "HAL/PIR.h"
-#include "HAL/Alarm.h"
-#include "HAL/RFID.h"
 
-#include "MCAL/MCAL.h"
-#include "HAL/HAL.h"
-
+#include "APP/SmartParking.h"
 
 int main(void)
 {
 	MCAL_init();
 	HAL_init();
-
-
-	while(1)
-	{
-
-		uint16_t x = RFID_exit_Read();
-		LCD_sendchar(&LCD_user, (unsigned char)x, DATA);
-	}
-
+	SmartParking_init();
+	SmartParking_main();
 }
