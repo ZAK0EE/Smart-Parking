00b326ae4279c65f89c785c41e20485dad027df8
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-07-26 18:05:59 +0300

    Added MCAL init function

---
+++ Inc/MCAL/MCAL.h
@@ -0,0 +1,14 @@
+/*
+ * MCAL.h
+ *
+ *  Created on: Jul 26, 2023
+ *      Author: Ziad Gamalelden
+ */
+
+#ifndef MCAL_MCAL_H_
+#define MCAL_MCAL_H_
+
+void MCAL_init(void);
+
+
+#endif /* MCAL_MCAL_H_ */
