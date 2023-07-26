d2dd6e7036b3ae8591a35cc4a75b3a02c6d76b69
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2023-07-27 01:09:39 +0300

    Added smartparking  init function

---
+++ Src/APP/SmartParking.c
@@ -0,0 +1,57 @@
+/*
+ * Smart Parking.c
+ *
+ *  Created on: Jul 26, 2023
+ *      Author: Ziad Gamalelden
+ */
+#include <stdio.h>
+
+#include "HAL/KPAD.h"
+#include "HAL/LCD.h"
+#include "HAL/RFID.h"
+#include "HAL/Alarm.h"
+#include "HAL/PIR.h"
+#include "HAL/Servo_Motor.h"
+
+#include "MCAL/Stm32_F103C6_Timer.h"
+
+static int8_t AuthenticIDs[3] = {-1, -1, -1};
+static uint8_t FreeSlots = 3;
+
+void SmartParking_init(void)
+{
+	// Admin enters authentic IDs
+	LCD_sendstring(&LCD_admin, (const char *)"Enter allowed ID");
+
+	char string[17];
+	for(int i = 0; i < 3; i++)
+	{
+		LCD_gotoxy(&LCD_admin, 0, i + 1);
+
+		sprintf(string, "Enter ID[%d]: ", i);
+		LCD_sendstring(&LCD_admin, (const char *)string);
+
+		do
+		{
+			AuthenticIDs[i] = KPAD_Get_Char();
+		}
+		while(AuthenticIDs[i] == 0);
+
+		LCD_sendchar(&LCD_admin, (unsigned char)AuthenticIDs[i], DATA);
+	}
+
+	// Shows entered IDs
+	LCD_clearscreen(&LCD_admin);
+	LCD_gotoxy(&LCD_admin, 0,0);
+
+	LCD_sendstring(&LCD_admin, (const char *)"Allowed IDs:");
+	for(int i = 0; i < 3; i++)
+	{
+		LCD_gotoxy(&LCD_admin, 0, i + 1);
+
+		sprintf(string, "ID[%d]: %c", i, AuthenticIDs[i]);
+		LCD_sendstring(&LCD_admin, (const char *)string);
+	}
+
+
+}
